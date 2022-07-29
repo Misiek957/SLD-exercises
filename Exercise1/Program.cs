517c10d5a271f6a9e701eee61fc0e55e0230bdb3
Mike Nahlik <michal.nahlik.edu@outlook.com>
2022-07-29 10:02:00 +0000

    Initial Commit

---
+++ Exercise1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World 2!");
+            Class1 HelloClass = new Class1();
+            Console.WriteLine(HelloClass.color);
+            Class1.Hello();
+        }
+    }
+
+}
+
+
