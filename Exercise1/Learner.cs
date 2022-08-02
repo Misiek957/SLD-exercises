9c85faba0b7e09dcd9fde4d9dd938971e99a3ce6
Mike Nahlik <michal.nahlik.edu@outlook.com>
2022-08-02 08:20:52 +0000

    code cleanup

--- Exercise1/Learner.cs
+++ Exercise1/Learner.cs
@@ -9,8 +9,11 @@ namespace Exercise1
     public class Learner
     {
         public string FamilyName { get; set; }
+
         public string GivenNames { get; set; }
+
         public string LearnRefNumber { get; set; }
+
         public int? Accom { get; set; }
 
         public static void Hello()
