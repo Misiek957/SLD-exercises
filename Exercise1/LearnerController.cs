75fb48ad045b38ad37bd94bc53a5d66d3847360d
Misiek <misiekleaim@gmail.com>
2022-08-03 09:40:20 +0000

    Exercise 3: code cleanup, make LearnerController class static

--- Exercise1/LearnerController.cs
+++ Exercise1/LearnerController.cs
@@ -4,23 +4,21 @@ namespace Exercise1
 {
     public class LearnerController
     {
-        string jsonFilenameHardcoded = "C:/dev/exercise/Exercise1/Exercise1/Learners/LearnersData.json"; // TODO: Hardcoded
-        string jsonFileNameTargetHardcoded = "C:/dev/exercise/Exercise1/Exercise1/Learners/ExportedLearnersData.json"; // TODO: Hardcoded
-        public List<Learner> ImportLearnerJson()
+        public static List<Learner> ImportLearnerJson()
         {
-            
-            //Console.WriteLine(jsonFilenameHardcoded);
-            string jsonText = File.ReadAllText(this.jsonFilenameHardcoded);
+            string jsonFilenameHardcoded = "C:/dev/exercise/Exercise1/Exercise1/Learners/LearnersData.json"; // TODO: Hardcoded, place in a static data file
+            string jsonText = File.ReadAllText(jsonFilenameHardcoded);
             var jsonService = new JsonSerializationService();
             var importedlearners = jsonService.Deserialize<List<Learner>>(jsonText);
             return importedlearners;
         }
 
-        public void ExportLearnerJson(List<Learner> learners)
+        public static void ExportLearnerJson(List<Learner> learners)
         {
+            string jsonFileNameTargetHardcoded = "C:/dev/exercise/Exercise1/Exercise1/Learners/ExportedLearnersData.json"; // TODO: Hardcoded, place in a static data file
             var jsonService = new JsonSerializationService();
             var jsonString = jsonService.Serialize(learners);
-            File.WriteAllText(this.jsonFileNameTargetHardcoded, jsonString);
+            File.WriteAllText(jsonFileNameTargetHardcoded, jsonString);
             Console.WriteLine("\nLearners data exported successfully  ");
         }
 
@@ -35,6 +33,38 @@ namespace Exercise1
             {
                 string displayName = "INVALID NAME";
                 return displayName;
+            }
+        }
+
+        private static Learner CreateCustomLearner(string firstName, string lastName, string reference, int accom)
+        {
+            var customLearner = new Learner
+            {
+                FamilyName = lastName,
+                GivenNames = firstName,
+                LearnRefNumber = reference,
+                Accom = accom
+            };
+
+            return customLearner;
+        }
+
+        public static List<Learner> CreateListOfCustomLearners()
+        {
+            var exportLearners = new List<Learner>();
+            Learner customLearner1 = CreateCustomLearner("Mary", "Sterling", "ref123", 1);
+            exportLearners.Add(customLearner1);
+            Learner customLearner2 = CreateCustomLearner("George", "Smith", "ref456", 0);
+            exportLearners.Add(customLearner2);
+
+            return exportLearners;
+        }
+
+        public static void ValidateListOflearners(List<Learner> listOfLearners)
+        {
+            foreach (var learner in listOfLearners)
+            {
+                Accom_1.CheckLearnerValidity(learner);
             }
         }
     }
--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -6,40 +6,15 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            // Import Learner json data
             Console.WriteLine("Starting...\n");
-            var learnerController = new LearnerController();
-
 
             // Import Learner json Data
-            var importedLearners = learnerController.ImportLearnerJson();
-            foreach (var learner in importedLearners)
-            {
-                Accom_1.CheckLearnerValidity(learner);
-            }
-
+            var importedLearners = LearnerController.ImportLearnerJson();
+            LearnerController.ValidateListOflearners(importedLearners);
 
             // Export Learner json Data
-            var exportLearners = new List<Learner>();
-            Learner customLearner1 = CreateCustomLearner("Mary", "Sterling", "ref123", 1);
-            exportLearners.Add(customLearner1);
-            Learner customLearner2 = CreateCustomLearner("George", "Smith", "ref456", 0);
-            exportLearners.Add(customLearner2);
-
-            learnerController.ExportLearnerJson(exportLearners);
-        }
-
-        private static Learner CreateCustomLearner(string firstName, string lastName, string reference, int accom)
-        {
-            var customLearner = new Learner
-            {
-                FamilyName = lastName,
-                GivenNames = firstName,
-                LearnRefNumber = reference,
-                Accom = accom
-            };
-
-            return customLearner;
+            var exportLearners = LearnerController.CreateListOfCustomLearners();
+            LearnerController.ExportLearnerJson(exportLearners);
         }
     }
 
